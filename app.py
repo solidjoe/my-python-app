e1c705bb21ec930ca35e9eba02b14e4bd531d0c7
Joel P John <164139412+solidjoe@users.noreply.github.com>
2026-01-02 12:11:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if st.session_state.step < 24:
    if current_block == "Projection":
        label = f"क्या आप निश्चित हैं कि {item['p']}?"
    else:
-        label = f"क्या यहाँ मुख्य बात यह है कि {item['p']}?"
+       label = f"क्या यहाँ मुख्य बात यह है कि {item['p']}?"
 
     # 0-100 Slider
     rating = st.slider(label, 0, 100, 50)
@@ -73,6 +73,7 @@ if st.session_state.step < 24:
 else:
 
     st.success("धन्यवाद! आपका डेटा सुरक्षित रूप से जमा हो गया है। (Thank you! Your data has been recorded.)")
+
 
 
 
